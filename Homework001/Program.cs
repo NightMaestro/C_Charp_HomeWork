ebf6fd7372e1a7788453ce857608b9b69af81a26
avraamdever <maestro@gmail.com>
2023-03-09 16:15:45 +0300

    Add homework1, Задание 1,2,3,4

---
+++ Homework001/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задание 1
+
+// Console.WriteLine("Задание 1, HomeWork 1");
+// Console.WriteLine("Найти большее из двух чисел введенных пользователем");
+// Console.Write("Введите первое число: ");
+// int a = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите второе число: ");
+// int b = int.Parse(Console.ReadLine()!);
+// if (a == b)
+// {
+//   Console.Write("Вы ввели одинаковые числа ");
+// }
+// if (a > b)
+// {
+//   Console.Write("Первое число БОЛЬШЕ второго ");
+// }
+// if (a < b)
+// {
+//   Console.Write("Первое число МЕНЬШЕ второго ");
+// }
+
+
+// Задание 2
+
+// Console.WriteLine("Задание 2, HomeWork 1");
+// Console.WriteLine("Найти большее из 3-х чисел");
+// Console.Write("Введите первое число: ");
+// int a = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите второе число: ");
+// int b = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите третье число: ");
+// int c = int.Parse(Console.ReadLine()!);
+// int max = a;
+// if (max<b)
+// {
+//   max=b;
+// }
+// if (max<c)
+// {
+//   max=c;
+// }
+// Console.Write($"Максимальное число = {max}");
+
+//Задание 3
+// Console.WriteLine("Задание 3, HomeWork 1");
+// Console.WriteLine("Проверка введенного числа на четность");
+// Console.Write("Введите число: ");
+// int a = int.Parse(Console.ReadLine()!);
+// if (a % 2 == 0)
+// {
+//   Console.Write($"Введенное число {a} - четное ");
+// }
+// else
+// {
+//   Console.Write($"Введенное число {a} - нечетное ");
+// }
+
+// Задание 4
+
+// Console.WriteLine("Задание 4, HomeWork 1");
+// Console.WriteLine("Вывод вcех четных чисел от 1 до N");
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.Write("Ряд чисел: ");
+// int count = 2;
+// while (count<n)
+// {
+//    Console.Write($"{count} ");
+//    count +=2;
+// }
